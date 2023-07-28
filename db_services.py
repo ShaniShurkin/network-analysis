1e061a77b54a4752eae7464704c5c88aad33c6d5
ShaniShurkin <shanishurkin@gmail.com>
2023-07-28 08:06:47 +0300

    add join to db_service

--- db_services.py
+++ db_services.py
@@ -1,3 +1,7 @@
+from typing import Dict, Tuple, Union
+
+from pydantic import BaseModel, Field
+
 from db_connection import connect
 from exceptions_handling import handle_db_exceptions
 import atexit
@@ -86,6 +90,47 @@ def create_dicts_list_from_rows(cursor):
     column_names = [column[0] for column in cursor.description]
     rows_lst = [dict(zip(column_names, row)) for row in cursor.fetchall()]
     return rows_lst
+
+
+class JoinStructure(BaseModel):
+    from_table: Tuple[str, str]
+    select: Tuple[Tuple[str, str], ...]
+    join: Tuple[Tuple[str, str], ...]
+    on: Tuple[Tuple[Tuple[str, Union[str, int]], ...], ...]
+
+
+# Should look like this:
+# join_vars = {"from_table": ("devices_connections", "dc"),
+#              "select": (
+#                  ("d1.mac_address", "src_mac_address"),
+#                  ("d2.mac_address", "dst_mac_address")),
+#              "join": (("devices", "d1"),
+#                       ("devices", "d2")),
+#              "on": (
+#                  (("dc.src_device_id", "d1.id"), ("network_id", 26)),
+#                  (("dc.dst_device_id", "d2.id"),)
+#              )
+#              }
+# js = JoinStructure(**join_vars)
+
+
+@handle_db_exceptions
+def join_tables(join_vars: JoinStructure):
+    with connection.cursor() as cursor:
+        # Should look like this:
+        # join_query = """SELECT d1.mac_address AS src_mac_address, d2.mac_address AS dst_mac_address
+        # FROM devices_connections dc
+        # JOIN devices d1 ON dc.src_device_id = d1.id AND d1.network_id=26
+        # JOIN devices d2 ON dc.dst_device_id = d2.id AND d2.network_id=26; """
+        selects = [f"{s1} AS {s2}" for s1, s2 in join_vars.select]
+        joins = [f"JOIN {j1} {j2} ON" for j1, j2 in join_vars.join]
+        ons = [" AND ".join([f"{on1} = {on2}" for on1, on2 in on]) for on in join_vars.on]
+        join_query = f"""SELECT {', '.join(selects)} \nFROM {join_vars.from_table[0]} {join_vars.from_table[1]}\n"""
+        for join, on in zip(joins, ons):
+            join_query += f"{join} {on}\n"
+        cursor.execute(join_query)
+        rows_lst = create_dicts_list_from_rows(cursor)
+        return rows_lst
 
 
 def close_connection():
